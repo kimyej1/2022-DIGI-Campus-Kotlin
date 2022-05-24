d7a939e86f56e20bd8b460ad0b0402aeab570df9
kimyej1 <yeji2501@gmail.com>
2022-05-24 17:16:42 +0900

    2022.05/24 17:16

--- src/main/kotlin/com/kbstar/kotlin/A45OperatorOverloading.kt
+++ src/main/kotlin/com/kbstar/kotlin/A45OperatorOverloading.kt
@@ -14,10 +14,15 @@ package com.kbstar.kotlin
  */
 
 class Point(var x:Int = 0, var y:Int = 0) {
-    operator fun plus(p:Point) : Point {
-        return Point(x + p.x, y + p.y)
+    operator fun plus(p:Point) : Point {        // plus, minus, times 이런 이름은 내가 정하는게 아니고
+        return Point(x + p.x, y + p.y)    // Operator Overloading 에 정해져있는 이름!
     }
-
+    operator fun minus(p:Point) : Point {
+        return Point(x - p.x, y - p.y)
+    }
+    operator fun times(p:Point) : Point {
+        return Point(x * p.x, y * p.y)
+    }
     override fun toString(): String {
         return "Point(x=$x, y=$y)"
     }
@@ -32,5 +37,11 @@ fun main() {
     println(point)
 
     point = p1.plus(p2) // same expression
+    println(point)
+
+    point = p1 - p2
+    println(point)
+
+    point = p1 * p2
     println(point)
 }
